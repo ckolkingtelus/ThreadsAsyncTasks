e94e8062c5dabbcce3353defdd9f7b1925f89481
Christian Kolking <christian.kolking@telus.com>
2018-12-18 13:26:29 -0500

    added Resource files but FAIL to use Integer resource value.

--- app/src/main/java/com/example/ckolking/appthreadtiming/MainActivity.java
+++ app/src/main/java/com/example/ckolking/appthreadtiming/MainActivity.java
@@ -10,9 +10,29 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import static java.lang.Boolean.*;
+
 public class MainActivity extends AppCompatActivity {
     TextView tv; //for class wide reference to update status
     Button bt; // for class wide reference to update button text
+    Integer nBtClicks; // for class wide reference to update number of button clicks
+    Boolean btBusy; // for class wide reference to update blocking state of button
+    Integer btJustify;
+    //TODO: cannot make the "integer" resource work neither here in declaration nor in constructor. 
+    // int countMax = getResources().getInteger(R.integer.maxNumberToProcess);
+    // instead hard-code:
+    Integer countMax = 10;
+
+    public MainActivity() {
+        nBtClicks = 0;
+        btBusy = FALSE;
+        // Log.d("CEK-MainActivity constructor", "countMax is " + countMax);
+        Log.d("CEK-MainActivity constructor", "Rid countMax is " + R.integer.maxNumberToProcess);
+        //Log.d("CEK-MainActivity constructor", "integer countMax is " + String.valueOf(countMax));
+        //Log.d("CEK-MainActivity constructor", "log stuff");
+        // countMax = Integer.valueOf(getString(R.integer.maxNumberToProcess));
+        //countMax = 10;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,18 +50,20 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             Log.d("CEK-doButtonClick", "UI message please wait");
             tv.setText("Processing, please wait.");
-            Log.d("CEK-doButtonClick", "CPU busy doing ThisTakesAWhile");
+            // Log.d("CEK-doButtonClick", "CPU busy doing ThisTakesAWhile");
             // BAD:  calls a function that runs CPU intensive stuff while the UI has to wait:
             // BAD: BAD_ThisTakesAWhile();
             // GOOD:  call the Async functionality...
             // TODO: call the Async stuff!
-            ThisTakesAWhile ttaw = new ThisTakesAWhile();
-            ttaw.execute();
-            Log.d("CEK-doButtonClick", "UI message finished");
-            // 'finished' only made sense before the Async task stuff
-            // tv.setText("Finished.");
-            // instead with the Async 'executed' in the background, then we can say that we have started: 
-            tv.setText("We have begun...");
+            if (!btBusy) {
+                ThisTakesAWhile ttaw = new ThisTakesAWhile();
+                ttaw.execute();
+                Log.d("CEK-doButtonClick", "UI message finished");
+                // 'finished' only made sense before the Async task stuff
+                // tv.setText("Finished.");
+                // instead with the Async 'executed' in the background, then we can say that we have started:
+                tv.setText("We have begun...");
+            }
         }
     }
 
@@ -55,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
             Log.d("CEK-ThisTakesAWhile", "... finished CPU hog.");
             count++;
             Log.d("CEK-ThisTakesAWhile", "UI message processed count " + count);
-            tv.setText("Processed " + count + " of 10.");
+            tv.setText("Processed " + count + " of " + getString(R.integer.maxNumberToProcess) + ".");
             Log.d("CEK-ThisTakesAWhile", "crash before here?");
         } while (count < 10);
     }
@@ -65,10 +87,22 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
+            nBtClicks +=1;
+            btBusy = TRUE;
+            // what alignment # is what?
+            // how do I get "set font / style to italics" ?
+            btJustify = bt.getTextAlignment();
+            bt.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START); // 2 = Left-just actually "START"
+            Log.d("CEK-test", "current is " + bt.getTextAlignment());
+            //bt.setText("..busy..");
+            bt.setText(R.string.btBusyText);
+            Log.d("CEK-onPreEx", "count of button clicks is " + nBtClicks);
         }
 
         @Override
         protected Integer doInBackground(Integer... integers) {
+            Log.d("CEK-DoInB-start", "count of button clicks is " + nBtClicks);
+
             // fails because calling a class member method is disliked by the OS:
             // FAILS: BAD_ThisTakesAWhile();
             // so instead, implement the code inside here directly:
@@ -77,17 +111,17 @@ public class MainActivity extends AppCompatActivity {
                 int count = 0;
                 do {
                     // this call is the CPU hog!!!
-                    Log.d("CEK-ThisTakesAWhile", "Starting CPU hog...");
+                    Log.d("CEK-DoInB-ThisTakesAWhile", "Starting CPU hog...");
                     SystemClock.sleep(1000);
-                    Log.d("CEK-ThisTakesAWhile", "... finished CPU hog.");
+                    Log.d("CEK-DoInB-ThisTakesAWhile", "... finished CPU hog.");
                     count++;
-                    Log.d("CEK-ThisTakesAWhile", "UI message processed count " + count);
-                    // FAILS here: tv.setText("Processed " + count + " of 10.");
+                    Log.d("CEK-DoInB-ThisTakesAWhile", "UI message processed count " + count);
+                    // FAILS here in 'doInBackgr': tv.setText("Processed " + count + " of 10.");
                     // instead, change UX elements in 'onProgressUpdate' method of the Async class:
                     publishProgress(count);
                     // yes this log works AFTER I put the 'textView' UX update in the Asynch 'progress update' method
-                    // Log.d("CEK-ThisTakesAWhile", "crash before here?");
-                } while (count < 10);
+                    // Log.d("CEK-DoInB-ThisTakesAWhile", "crash before here?");
+                } while (count < countMax);
             //}
             return count;
         }
@@ -95,19 +129,27 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Integer integer) {
             super.onPostExecute(integer);
+            btBusy = FALSE;
+            bt.setTextAlignment(btJustify);
+            //bt.setText("GO");
+            bt.setText(R.string.btReadyText);
+            Log.d("CEK-onPostEx", "count of button clicks is " + nBtClicks);
         }
 
         @Override
         protected void onProgressUpdate(Integer... values) {
             super.onProgressUpdate(values);
-            // FAILS here: tv.setText("Processed " + count + " of 10.");
+            Log.d("CEK-onProgressUpd", "count of button clicks is " + nBtClicks);
+
+            // to fix FAILS here in 'doInBackgr': tv.setText("Processed " + count + " of " + R.integer.maxNumberToProcess + ".");
             // fix:
-            tv.setText("Processed " + values[0] + " of 10.");
+            tv.setText("Processed " + values[0] + " of " + getString(R.integer.maxNumberToProcess) + ".");
         }
 
         @Override
         protected void onCancelled() {
             super.onCancelled();
+            Log.d("CEK-onCancelled", "count of button clicks is " + nBtClicks);
         }
     }
 }
