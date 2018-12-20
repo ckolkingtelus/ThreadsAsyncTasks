47178cf8b2f4048e984abcef164c731fb8eda301
Christian Kolking <christian.kolking@telus.com>
2018-12-19 19:53:39 -0500

    1. Added 'settings' activity;  2. Changed Log.d tags to "TAG" variable.

--- app/src/main/java/com/example/ckolking/appthreadtiming/MainActivity.java
+++ app/src/main/java/com/example/ckolking/appthreadtiming/MainActivity.java
@@ -24,12 +24,13 @@ public class MainActivity extends AppCompatActivity {
     Integer countMax = 10;
 
     public MainActivity() {
+        final String TAG = "CEK-MainActivity constructor";
         nBtClicks = 0;
         btBusy = FALSE;
-        // Log.d("CEK-MainActivity constructor", "countMax is " + countMax);
-        Log.d("CEK-MainActivity constructor", "Rid countMax is " + R.integer.maxNumberToProcess);
-        //Log.d("CEK-MainActivity constructor", "integer countMax is " + String.valueOf(countMax));
-        //Log.d("CEK-MainActivity constructor", "log stuff");
+        // Log.d(TAG, "countMax is " + countMax);
+        Log.d(TAG, "Rid countMax is " + R.integer.maxNumberToProcess);
+        //Log.d(TAG, "integer countMax is " + String.valueOf(countMax));
+        //Log.d(TAG, "log stuff");
         // countMax = Integer.valueOf(getString(R.integer.maxNumberToProcess));
         //countMax = 10;
     }
@@ -37,20 +38,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final String TAG = "CEK-onCreate";
         setContentView(R.layout.activity_main);
         //get the references to on screen items
         tv = (TextView) findViewById(R.id.textView);
         bt = (Button) findViewById(R.id.button);
         //handle button presses
-        Log.d("CEK-onCreate", "setOnClickListener");
+        Log.d(TAG, "setOnClickListener");
         findViewById(R.id.button).setOnClickListener(new doButtonClick());
     }
 
     class doButtonClick implements View.OnClickListener {
         public void onClick(View v) {
-            Log.d("CEK-doButtonClick", "UI message please wait");
+            final String TAG = "CEK-doButtonClick";
+
+            Log.d(TAG, "UI message please wait");
             tv.setText("Processing, please wait.");
-            // Log.d("CEK-doButtonClick", "CPU busy doing ThisTakesAWhile");
+            // Log.d(TAG, "CPU busy doing ThisTakesAWhile");
             // BAD:  calls a function that runs CPU intensive stuff while the UI has to wait:
             // BAD: BAD_ThisTakesAWhile();
             // GOOD:  call the Async functionality...
@@ -59,28 +63,28 @@ public class MainActivity extends AppCompatActivity {
             if (!btBusy) {
                 ThisTakesAWhile ttaw = new ThisTakesAWhile();
                 ttaw.execute();
-                Log.d("CEK-doButtonClick", "UI message finished");
+                Log.d(TAG, "UI message finished");
                 // 'finished' only made sense before the Async task stuff
                 // tv.setText("Finished.");
                 // instead with the Async 'executed' in the background, then we can say that we have started:
                 tv.setText("We have begun...");
             }
-
         }
     }
 
     private void BAD_ThisTakesAWhile() {
         //mimic long running code
+        final String TAG = "CEK-ThisTakesAWhile";
         int count = 0;
         do {
             // this call is the CPU hog!!!
-            Log.d("CEK-ThisTakesAWhile", "Starting CPU hog...");
+            Log.d(TAG, "Starting CPU hog...");
             SystemClock.sleep(1000);
-            Log.d("CEK-ThisTakesAWhile", "... finished CPU hog.");
+            Log.d(TAG, "... finished CPU hog.");
             count++;
-            Log.d("CEK-ThisTakesAWhile", "UI message processed count " + count);
+            Log.d(TAG, "UI message processed count " + count);
             tv.setText("Processed " + count + " of " + getString(R.integer.maxNumberToProcess) + ".");
-            Log.d("CEK-ThisTakesAWhile", "crash before here?");
+            Log.d(TAG, "crash before here?");
         } while (count < 10);
     }
 
@@ -89,21 +93,24 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
+            final String TAG = "CEK-onPreEx";
             nBtClicks +=1;
             btBusy = TRUE;
             // what alignment # is what?
             // how do I get "set font / style to italics" ?
             btJustify = bt.getTextAlignment();
             bt.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START); // 2 = Left-just actually "START"
-            Log.d("CEK-test", "current is " + bt.getTextAlignment());
+            //Log.d("CEK-test", "current is " + bt.getTextAlignment());
             //bt.setText("..busy..");
             bt.setText(R.string.btBusyText);
-            Log.d("CEK-onPreEx", "count of button clicks is " + nBtClicks);
+            Log.d(TAG, "count of button clicks is " + nBtClicks);
         }
 
         @Override
         protected Integer doInBackground(Integer... integers) {
-            Log.d("CEK-DoInB-start", "count of button clicks is " + nBtClicks);
+            final String TAG = "CEK-DoInB-start";
+
+            Log.d(TAG, "count of button clicks is " + nBtClicks);
 
             // fails because calling a class member method is disliked by the OS:
             // FAILS: BAD_ThisTakesAWhile();
@@ -113,16 +120,16 @@ public class MainActivity extends AppCompatActivity {
                 int count = 0;
                 do {
                     // this call is the CPU hog!!!
-                    Log.d("CEK-DoInB-ThisTakesAWhile", "Starting CPU hog...");
+                    Log.d(TAG, "Starting CPU hog...");
                     SystemClock.sleep(1000);
-                    Log.d("CEK-DoInB-ThisTakesAWhile", "... finished CPU hog.");
+                    Log.d(TAG, "... finished CPU hog.");
                     count++;
-                    Log.d("CEK-DoInB-ThisTakesAWhile", "UI message processed count " + count);
+                    Log.d(TAG, "UI message processed count " + count);
                     // FAILS here in 'doInBackgr': tv.setText("Processed " + count + " of 10.");
                     // instead, change UX elements in 'onProgressUpdate' method of the Async class:
                     publishProgress(count);
                     // yes this log works AFTER I put the 'textView' UX update in the Asynch 'progress update' method
-                    // Log.d("CEK-DoInB-ThisTakesAWhile", "crash before here?");
+                    // Log.d(TAG, "crash before here?");
                 } while (count < countMax);
             //}
             return count;
@@ -131,17 +138,21 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Integer integer) {
             super.onPostExecute(integer);
+            final String TAG = "CEK-onPostEx";
+
             btBusy = FALSE;
             bt.setTextAlignment(btJustify);
             //bt.setText("GO");
             bt.setText(R.string.btReadyText);
-            Log.d("CEK-onPostEx", "count of button clicks is " + nBtClicks);
+            Log.d(TAG, "count of button clicks is " + nBtClicks);
         }
 
         @Override
         protected void onProgressUpdate(Integer... values) {
             super.onProgressUpdate(values);
-            Log.d("CEK-onProgressUpd", "count of button clicks is " + nBtClicks);
+            final String TAG = "CEK-onProgressUpd";
+
+            Log.d(TAG, "count of button clicks is " + nBtClicks);
 
             // to fix FAILS here in 'doInBackgr': tv.setText("Processed " + count + " of " + R.integer.maxNumberToProcess + ".");
             // fix:
@@ -151,7 +162,9 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onCancelled() {
             super.onCancelled();
-            Log.d("CEK-onCancelled", "count of button clicks is " + nBtClicks);
+            final String TAG = "CEK-onCancelled";
+
+            Log.d(TAG, "count of button clicks is " + nBtClicks);
         }
     }
 }
